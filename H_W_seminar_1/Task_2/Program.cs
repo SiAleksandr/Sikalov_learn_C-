665eed0659ef581c93ddaa922338c59eaa023cf8
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-14 19:32:55 +0300

    Add first text of Task_1 from H_W_seminar1

--- H_W_seminar_1/Task_2/Program.cs
+++ H_W_seminar_1/Task_2/Program.cs
@@ -3,4 +3,19 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
-Console.WriteLine("Hello, World!");
+System.Console.Write("Введите первое число -> ");
+int first = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число -> ");
+int second = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третее число -> ");
+int third = Convert.ToInt32(Console.ReadLine());
+int max = first;
+if ( second > first ) {
+    max = second;
+}
+else {
+    if ( third > first ) {
+        max = third;
+    }
+}
+System.Console.Write($"Наибольшее число это: {max}"};
