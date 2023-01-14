3dd4cc983f7bdbd9c20f54097a7f7199a42381d1
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-14 18:09:48 +0300

    Add exenple 01 of Task_1 from H_W_seminar_1

--- H_W_seminar_1/Task_1/Program.cs
+++ H_W_seminar_1/Task_1/Program.cs
@@ -4,4 +4,17 @@
 //a = 5; b = 7 -> max = 7, min = 5
 //a = -9 b = -3 -> max = -3, min = -9
 System.Console.Write("Введите первое число -> ");
-int nomber1 = Convert.ToInt32(Console.ReadLine());
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число -> ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if ( number1 > number2 ) {
+    System.Console.WriteLine($"Большее = {number1}; Меньшее = {number2}"); 
+}
+else {
+    if ( numder1 == number2 ) {
+        System.Console.WriteLine($"Эти числа равны {number1} = {number2}";
+    }
+    else {
+        System.Console.WriteLine($"Большее = {number2}; Меньшее = {number1}");
+    }
+}
