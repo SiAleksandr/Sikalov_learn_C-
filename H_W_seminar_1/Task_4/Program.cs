a58330d6af0d56d192daf8a4e07d886edbcc2605
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-15 01:01:46 +0300

    Task_4 from H_W_seminer_1 is ready

---
+++ H_W_seminar_1/Task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+System.Console.Write("Введите число -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 1) {
+    int count = 0;
+    int sum = 0;
+    System.Console.WriteLine($"Чётные числа от 0 до {number} это:");
+    number = number / 2;
+    while ( count < number ) {
+        sum = sum + 2;
+        System.Console.Write($"{sum}, ");
+        count +=1;
+    }
+}
+else {
+    System.Console.WriteLine($"Число {number} не подходит");   
+}
