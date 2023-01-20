8461e2994263e12acf052eaaef2f9019f5f74c92
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-21 02:05:39 +0300

    Begin of Task 1 from HW seminar 3

---
+++ H_W_seminar_3/Task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+int GetGivenNumber(int N) 
+{
+    System.Console.WriteLine("Введите для проверки число -> ");
+    N = Convert.ToInt32(Console.ReadLine());
+    int Num = N;
+    return Num;
+}
+int firstNum = 24;
+int answer = GetGivenNumber(firstNum);
+System.Console.WriteLine(answer);
+// Пока что это белеберда
