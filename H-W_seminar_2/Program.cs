f22dc72d48e629c48811eb08878d3d9b093270b0
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-20 11:49:30 +0300

    Task 1 from seminar 2 is ready

--- H-W_seminar_2/Program.cs
+++ H-W_seminar_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает
+//вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+System.Console.Write("Ввудите трёхзначное число -> ");
+int givenNum = Convert.ToInt32(Console.ReadLine());
+if (givenNum > 99 && givenNum < 1000)
+{
+    int FindDigit2(int x3)
+    {
+        int dig2 = (x3 / 10) % 10;
+        return dig2;
+    }
+    int answer = FindDigit2(givenNum);
+    System.Console.WriteLine($"Вторая цифра этого числа это {answer}");
+}
+else
+{
+    System.Console.WriteLine("Это число не подходит");
+}
