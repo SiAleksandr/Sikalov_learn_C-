dac4f946347bdd0439af3915e9ea96e1377e2669
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-20 16:07:41 +0300

    Task_3 from H_W_seminar_2 is ready

---
+++ H-W_seminar_2/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является
+//ли этот день выходным. 
+//6 -> да
+//7 -> да
+//1 -> нет
+System.Console.Write("Введите, цифру, обозначающую день недели -> ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 0 && number <= 7)
+{
+    void CheckingWeekend(int day)
+    {
+        if (day > 5)
+        {
+            System.Console.WriteLine("Да это же выходной!");
+        }   
+        else 
+        {
+            System.Console.WriteLine("Нет уж, это будний день.");
+        }    
+    }
+    CheckingWeekend(number);
+}
