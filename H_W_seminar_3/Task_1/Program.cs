f2a3993e3fc24736c85310b23998d2f07e266a61
Sikalov Aleksandr <sikalov-aleksandr@mail.ru>
2023-01-22 16:21:08 +0300

    Task_1 from H_W_seminar_3 is ready again

--- H_W_seminar_3/Task_1/Program.cs
+++ H_W_seminar_3/Task_1/Program.cs
@@ -1,15 +1,131 @@
-﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿//Напишите программу, которая принимает на вход
+//пятизначное число и проверяет,
+//является ли оно палиндромом.
 //14212 -> нет
 //12821 -> да
 //23432 -> да
-int GetGivenNumber(int N) 
-{
-    System.Console.WriteLine("Введите для проверки число -> ");
-    N = Convert.ToInt32(Console.ReadLine());
-    int Num = N;
-    return Num;
+// Я сделал для любого числа
+int CountDigits(int k)     // функция считает, сколько цифр
+{                          // в любом числе k 
+    int count = 0;
+    if (k == 0)
+    {
+        count = 1;
+    }                        
+    if (k < 0)
+    {
+        k = (k * (-1));
+    }                            
+    while (k > 0)
+        {
+            k = k / 10;
+            count +=1;
+        }
+    return count;
 }
-int firstNum = 24;
-int answer = GetGivenNumber(firstNum);
-System.Console.WriteLine(answer);
-// Пока что это белеберда
+System.Console.Write("Палиндром, или нет это число -> ");
+int givenNum = Convert.ToInt32(Console.ReadLine());
+int GetDiget(int N, int No)          // Функция определяет, цифру
+{                                    // в числе N по номеру её разряда
+    if (N < 0)
+    {
+        N = (N * (-1));
+    }                                 
+    int count = 0;
+    int digit = -1;
+    while (count <= No)
+    {
+        count ++;
+        if (count == No)
+        {
+            digit = N % 10;
+            break;
+        }
+        N = N / 10;
+    }
+        return digit;
+}
+bool answer = true;
+int lenght = CountDigits(givenNum);
+if (lenght == 1)
+{
+    answer = true;
+}
+else
+{
+    int left = -1;
+    int right = -1;
+    int center = -1;
+    int centerDigit = -1;
+    int leftDigit = -1;
+    int rightDigit = -1;
+    int oldLeft = -1;
+    int oldRight = -1;
+    int i = -1;
+    if ((lenght % 2) == 1)
+    {
+        center = (lenght / 2) +1;
+        left = center;
+        right = center;
+        centerDigit = GetDiget(givenNum, center);
+        leftDigit = GetDiget(givenNum, left);
+        rightDigit = GetDiget(givenNum, right);
+        i = center;
+        while (i > 1)
+        {
+            oldLeft = leftDigit;
+            oldRight = rightDigit;
+            left = left - 1;
+            right = right + 1;
+            leftDigit = GetDiget(givenNum, left);
+            rightDigit = GetDiget(givenNum, right);
+            if ((centerDigit > leftDigit) && (leftDigit == rightDigit) && (leftDigit < oldLeft) && (rightDigit < oldRight))
+            {
+                answer = true;
+            }
+            else 
+            {
+                answer = false;
+                break;
+            }
+            i = i - 1;
+
+        }
+        
+    }
+    else
+    {
+        left = lenght / 2;
+        right = (lenght / 2) + 1;
+        leftDigit = GetDiget(givenNum, left);
+        rightDigit = GetDiget(givenNum, right);
+        i = left;
+        while (i > 1)
+        {
+            oldLeft = leftDigit;
+            oldRight = rightDigit;
+            left = left - 1;
+            right = right + 1;
+            leftDigit = GetDiget(givenNum, left);
+            rightDigit = GetDiget(givenNum, right);
+            if ((leftDigit == rightDigit) && (leftDigit < oldLeft) && (rightDigit < oldRight))
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+                break;
+            }
+            i = i - 1;
+        }
+    }
+}   
+if (answer == true)
+{
+        System.Console.WriteLine("Да, палиндром");   
+}
+else
+{                                                       
+    System.Console.WriteLine("Нет, не палиндром");
+}
---
+++ H_W_seminar_3/Task_2/Program.cs
@@ -0,0 +1,79 @@
+﻿// Напишите программу, которая принимает на вход
+//координаты двух точек и находит расстояние между
+//ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+System.Console.WriteLine("Введите координаты точки A");
+System.Console.Write("X = ");
+int xA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("y = ");
+int yA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("z = ");
+int zA = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координаты точки B");
+System.Console.Write("X = ");
+int xB = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("y = ");
+int yB = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("z = ");
+int zB = Convert.ToInt32(Console.ReadLine());
+int Modulo(int N)
+{
+    int modN = 0;
+    if (N < 0)
+    {
+        modN = N * (-1);
+    }
+    else
+    {
+        modN = N;
+    }
+    return modN;
+}
+int FindMax(int n0, int n1, int n2, int n3, int n4, int n5)
+{
+    int[] coord = new int[6];
+    coord[0] = n0;
+    coord[1] = n1;
+    coord[2] = n2;
+    coord[3] = n3;
+    coord[4] = n4;
+    coord[5] = n5;
+    int maxCoord = coord[0];
+    int i = 0;
+    while(i < 6)
+    {
+        if (coord[i] > maxCoord)
+        {
+            maxCoord = coord[i];
+        }
+        i++;
+    }
+    return maxCoord;
+}
+double LenghtAB(int startXa, int startYa, int startZa, int startXb, int startYb, int startZb)
+{
+    int modXa = Modulo(startXa);
+    int modYa = Modulo(startYa);
+    int modZa = Modulo(startZa);
+    int modXb = Modulo(startYb);
+    int modYb = Modulo(startYb);
+    int modZb = Modulo(startZb);
+    int max = FindMax(modXa, modYa, modZa, modXb, modYb, modZb);
+    int newXa = max + startXa;
+    int newYa = max + startYa;
+    int newZa = max + startZa;
+    int nweXb = max + startXb;
+    int newYb = max + startYb;
+    int newZb = max + startZb;
+    int diffYaYb = newYa - newYb;
+    diffYaYb = Modulo(diffYaYb);
+    int diffXaXb = newXa - nweXb;
+    diffXaXb = Modulo(diffXaXb);
+    int diffZaZb = newZa - newZb;
+    double gipotenuzaOxOz = Math.Sqrt((diffXaXb * diffXaXb) + (diffZaZb * diffZaZb));
+    double ab = Math.Sqrt((gipotenuzaOxOz * gipotenuzaOxOz) + (diffYaYb * diffXaXb));
+    return ab;
+}
+double answer = LenghtAB(xA, yA, zA, xB, yB, zB);
+System.Console.Write($"Расстояние от A до B = {answer}");
